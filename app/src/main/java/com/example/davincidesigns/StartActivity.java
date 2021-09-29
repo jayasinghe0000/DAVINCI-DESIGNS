83a745acb170b406d7dbd3f2ebd71e7c4629f5f1
jayasinghe0000 <it20241650@my.sliit.lk>
2021-09-29 17:00:00 +0530

    updated

--- app/src/main/java/com/example/davincidesigns/StartActivity.java
+++ app/src/main/java/com/example/davincidesigns/StartActivity.java
@@ -21,8 +21,8 @@ public class StartActivity extends AppCompatActivity {
         asVisitor.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-               Intent i = new Intent(StartActivity.this,CategoryDisplayActivity.class);
-                startActivity(i);
+              // Intent i = new Intent(StartActivity.this,CategoryDisplayActivity.class);
+               // startActivity(i);
             }
         });
 
